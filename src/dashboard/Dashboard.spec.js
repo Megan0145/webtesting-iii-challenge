7dec06618802ff1499109f612ed4aa66d91a531b
Megan <mennis0145@gmail.com>
2019-10-14 12:45:12 +0100

    Test: gate defaults to unlocked, Test: gate defaults to open on first render

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+afterEach(rtl.cleanup);
+let wrapper;
+beforeEach(() => {
+    wrapper = rtl.render(<Dashboard />);
+});
+
+describe('Gate', () => {
+    it('defaults to `unlocked`', () => {
+        expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    });
+    it('defaults to `open`', () => {
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    });
+});
